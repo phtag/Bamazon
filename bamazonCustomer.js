35822406d740182db8eb69ff2efb3edc8ef82920
Peter Tag <peter.h.tag@gmail.com>
2019-03-11 16:23:19 -0700

    Updates for activity 1

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -43,5 +43,20 @@ function getNumberOfUnitsDesired(productID) {
     message: "Please enter the number of units you want to buy for product ID=" + productID + ":",
     })
     .then(function(answer) {
+        connection.query("SELECT stock_quantity FROM products WHERE item_id=" + productID, function(err, res) {
+            var currentInventoryLevel = Number(res[0].stock_quantity);
+            var purchaseQuantity = Number(answer.units);
+            if (currentInventoryLevel < purchaseQuantity) {
+                console.log('Your order exceeds the units available for purchase');
+            } else {
+                connection.query("UPDATE products SET stock_quantity=" + (currentInventoryLevel-purchaseQuantity) + ' WHERE item_id=' + productID, function(err, res) {
+                    connection.query("SELECT * FROM products", function(err, res) {
+                        if (err) throw err;
+                        console.table(res);
+                        getProductID();
+                    });                    
+                });
+            }
+        });
     });
 }
