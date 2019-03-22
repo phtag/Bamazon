2cb3a055b9a17aca07e271b62a9873ac89f116ac
Peter Tag <peter.h.tag@gmail.com>
2019-03-22 11:59:07 -0700

    Update manager logic

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -24,17 +24,15 @@ function getManagerAction() {
     .prompt({
     name: "action",
     type: "list",
-    choices: ['View Products for Sale',
-            'View Low Inventory',
-            'Add to Inventory',
-            'Add New Product'],
+    choices: ['View Product Sales by Department',
+            'Create New Department'],
     })
     .then(function(answer) {
         console.log(answer.action);
         switch (answer.action) {
             case 'View Products for Sale':
 
-                connection.query("SELECT item_id, product_name, department_name, price, stock_quantity FROM products", function(err, res) {
+                connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function(err, res) {
                     if (err) {
                         throw err;
                     }
@@ -75,7 +73,7 @@ function getProductID() {
     .prompt({
     name: "item_id",
     type: "input",
-    message: "Please enter the ID of the product you want to add inventory to:",
+    message: "Please enter the ID of the product you to add inventory to:",
     })
     .then(function(answer) {
         getUnitsToItem(answer.item_id);
@@ -108,54 +106,41 @@ function addUnitsToItem(item_id, units_to_add) {
 function addNewProduct() {
     inquirer
     .prompt({
-        name: "product_name",
-        type: "input",
-        message: "Please enter the name of the new product:",
+    name: "product_name",
+    type: "input",
+    message: "Please enter the name of the new product:",
     })
     .then(function(answer) {
         product_name = answer.product_name;
-        console.log("Please select a department for product='" + product_name);
-        connection.query("SELECT department_name FROM products GROUP BY department_name", function(err, res) {
-            // Make the user select the department for the new product
-            var departments = res.map(obj => obj.department_name);
-            inquirer
+        inquirer
             .prompt({
-                name: "action",
-                type: "list",
-                choices: departments,
-             })
+            name: "price",
+            type: "input",
+            message: "Please enter the price of product '" + product_name + "':",
+            })
             .then(function(answer) {
-                // Get the price of the new product
-                var department_name = answer.action; 
+                price = answer.price;
                 inquirer
-                .prompt({
-                    name: "price",
+                    .prompt({
+                    name: "units",
                     type: "input",
-                    message: "Please enter the price of product '" + product_name + "' in department='" + department_name + "':",
-                })
-                .then(function(answer) {
-                    //  Get the number of units to add to inventory for this new product
-                    price = answer.price;
-                    inquirer
-                        .prompt({
-                            name: "units",
-                            type: "input",
-                            message: "Please enter the initial number of units to add to inventory for product='" + product_name + "':",
+                    message: "Please enter the initial number of units to add to inventory for product='" + product_name + "':",
                     })
                     .then(function(answer) {
-                        //  Add new product to the database
+                        console.log("Got here for product=" + product_name);
                         units = answer.units;
                         connection.query("SELECT COUNT(*) as numRecords FROM products", function(err, res) {
                             // Don't really need this, but was just curious about how to get number of records in a table
                             // in case the ID field was NOT auto-generated
                             var numRecords = res[0].numRecords;
 
-                            connection.query("INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('" + product_name + "','" + department_name + "'," + price + "," + units + ")", function(err, res) {
+                            console.log('numRecords='+numRecords);
+                            connection.query("INSERT INTO products (product_name, price, stock_quantity) VALUES ('" + product_name + "'," + price + "," + units + ")", function(err, res) {
                                     if (err) {
                                     console.log(err);
                                     throw err;
                                 }
-                                connection.query("SELECT item_id, product_name, department_name, price, stock_quantity FROM products", function(err, res) {
+                                connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function(err, res) {
                                     console.log('Viewing products...')
                                     console.table(res);
                                     getManagerAction();
@@ -163,8 +148,6 @@ function addNewProduct() {
                             });
                         });
                     });
-                });
             });
-        });
     });
 }
